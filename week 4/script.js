2251f6c2d61b2dc322526d038a814c05495a8937
jessica banka <jessicabanka3081@gmail.com>
2021-09-29 18:04:28 +0530

    Space

--- week 4/script.js
+++ week 4/script.js
@@ -13,7 +13,7 @@ names[10]="jim";
 
 for (var i = 0; i <names.length; i++) {
 	if(names[i].charAt(0)=='J'||names[i].charAt(0)=='j'){
-	console.log("Goodbye "+ names[i]);
+	console.log("Good Bye "+ names[i]);
 
 }
 else
