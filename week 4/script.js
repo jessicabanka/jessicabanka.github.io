2ebed61d50d073e4503abd3fc55101616c60b2a4
jessica banka <jessicabanka3081@gmail.com>
2021-09-29 18:01:28 +0530

    names

--- week 4/script.js
+++ week 4/script.js
@@ -1,15 +1,15 @@
 var names=new Array();
-names[0]="Yashika";
-names[1]="Jessica";
+names[0]="Yaakov";
+names[1]="John";
 names[2]="Jason";
-names[3]="Jenni";
-names[4]="john";
-names[5]="priya";
-names[6]="franklin";
-names[7]="lakshita";
-names[8]="pauli";
-names[9]="lauren";
-names[10]="jerry";
+names[3]="Jen";
+names[4]="jason";
+names[5]="paul";
+names[6]="frank";
+names[7]="larry";
+names[8]="paula";
+names[9]="laura";
+names[10]="jim";
 
 for (var i = 0; i <names.length; i++) {
 	if(names[i].charAt(0)=='J'||names[i].charAt(0)=='j'){
